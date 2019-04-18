c8a30a75d7da1f2edc4337a37a3804200f3d5c49
alexzanderk <kotsarev.a@gmail.com>
2019-04-18 17:58:39 +0300

    ts types

---
+++ index.ts
@@ -0,0 +1,18 @@
+const isOpen: boolean = false;
+const age: number = 155;
+const myName: string = 'Alex';
+const list: number[] = [1, 2, 3, 4];
+const me: [string, number, boolean] = ['Alex', 20000, true];
+
+enum Job {
+    webDev,
+    Front,
+    Back
+}
+
+const job: Job = Job.webDev;
+
+
+const phone: any = "iPhone";
+
+const never
